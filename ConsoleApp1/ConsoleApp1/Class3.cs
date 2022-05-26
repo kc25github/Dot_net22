4a6b8db7e0a266448d93a7036d518a3c49beafbb
kc25github <90677175+kc25github@users.noreply.github.com>
2022-05-27 00:21:53 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class3
+    {
+        static void Main(string[] args)
+        {
+            //for (int row = 0; row < 5; row++)
+            //{
+            //    for (int col = 0; col < 5; col++)
+            //    {
+            //        Console.WriteLine("row: "+row+ "col: "+col);
+            //    }
+            //}
+
+            for (int row = 0; row <= 9; row++)
+            {
+                for (int col = 0; col <= 9; col++)
+                {
+                    if (row == 0 || col == 0 || row == 9 || col == 9 || row == col || col == 9 - row + 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
