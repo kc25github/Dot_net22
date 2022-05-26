7ace842dff5ea400b5b1f2c58874229fc2b23388
kc25github <90677175+kc25github@users.noreply.github.com>
2022-05-26 23:53:03 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main2(string[] args)
+        {
+
+            //<data-type>  <variable-name>
+            int a = 5;
+            int b = 5;
+         
+
+
+            //whether a number is positive or not
+            int positiveNumber = 0;
+            Console.WriteLine();
+            if (positiveNumber > 0)
+            {
+                Console.WriteLine(positiveNumber + " is a positive Number");
+            }
+            else if (positiveNumber < 0)
+            {
+                Console.WriteLine(positiveNumber + " is not a positive Number");
+            }
+            else
+            {
+                Console.WriteLine(positiveNumber + " is neither postive nor negative");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class2
+    {
+        public static void Main()
+        {
+
+            int[,] arr1 = new int[2, 2];
+            int[,] arr2 = new int[2, 2];
+            //int[,] arr3 = new int[2, 2];
+
+            //Input from arr1
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.WriteLine("Please enter the value for arr1[" + row + "," + col + "]: ");
+                    arr1[row, col] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            //Input from arr2
+            //for (int row = 0; row < 2; row++)
+            //{
+            //    for (int col = 0; col < 2; col++)
+            //    {
+            //        Console.WriteLine("Please enter the value for arr2[" + row + "," + col + "]: ");
+            //        arr2[row, col] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+
+            Console.WriteLine("Matrix before Transpose");
+            //Matrix before Transpose
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    //Console.Write((arr1[row, col]+arr2[row,col])+"\t");
+                    Console.Write(arr1[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            //process for transpose
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    arr2[col, row] = arr1[row, col];
+                }
+            }
+            Console.WriteLine("Matrix After Transpose");
+            //Output
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    //Console.Write((arr1[row, col]+arr2[row,col])+"\t");
+                    Console.Write(arr2[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
